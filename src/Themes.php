73b92ec55d0f6bf0703173dc9e7ce06c7025e5aa
Keith Freeman <percival@code550.info>
2017-04-15 00:09:09 -0400

    first commit

---
+++ src/Themes.php
@@ -0,0 +1,30 @@
+<?php namespace DocGen;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Themes extends Command
+{
+    protected $config;
+
+    protected function configure()
+    {
+        $this
+            ->setName('themes')
+            ->setDescription('Show a list of available themes');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $list = "<info>Available Themes:</info>\n";
+        foreach (glob(__DIR__ . DIRECTORY_SEPARATOR . 'themes' . DIRECTORY_SEPARATOR . '*', GLOB_ONLYDIR) as $theme) {
+            $theme = basename($theme);
+            if ($theme == 'docgen') {
+                $theme = 'docgen (base)';
+            }
+            $list .= "<comment>$theme</comment>\n";
+        }
+        $output->write($list);
+    }
+}
